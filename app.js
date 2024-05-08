411b39f96c76e875c95eb554317c92a2cbe01c83
A Beetle <abeetle15@gmail.com>
2024-05-07 22:08:04 -0500

    Empezar a jugar con la página.
    explorar interacción de los botones, posibles animaciones de CSS, etc.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('ruiditos cheveres')
+const goHome = document.querySelector('.contGoHome')
+const goHomeText = document.querySelector('.goHomeText')
+const titulo = document.querySelector('.contTitulo')
+const subtitulo = document.querySelector('.subtitulo')
+
+
+
+goHome.addEventListener('mouseenter', () => {
+  goHomeText.classList.toggle('hover')
+  console.log(goHomeText.classList)
+})
+goHome.addEventListener('mouseleave', () => {
+  goHomeText.classList.toggle('hover')
+  console.log(goHomeText.classList)
+})
+titulo.addEventListener('mouseenter', () => {
+  subtitulo.classList.toggle('hover')
+  console.log(subtitulo.classList)
+})
+titulo.addEventListener('mouseleave', () => {
+  subtitulo.classList.toggle('hover')
+  console.log(subtitulo.classList)
+})
